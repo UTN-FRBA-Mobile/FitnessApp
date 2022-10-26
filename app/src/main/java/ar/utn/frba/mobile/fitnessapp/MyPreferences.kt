4cdfdcd72b44c3b2ce4c25581a6347e37174adb6
facuhumpTenpo <facundo.humphreys@tenpo.cl>
2022-10-25 21:01:27 -0300

    Adding notifications and removing visibility of components

--- app/src/main/java/ar/utn/frba/mobile/fitnessapp/MyPreferences.kt
+++ app/src/main/java/ar/utn/frba/mobile/fitnessapp/MyPreferences.kt
@@ -6,9 +6,25 @@ import android.content.SharedPreferences
 
 object MyPreferences {
     private const val showBGsKey = "preference_is_show_backgrounds"
+    private val PREF_NAME = "PREF_NAME"
+    private val FIREBASE_TOKEN = "FIREBASE_TOKEN"
 
     private fun getPreferences(context: Context): SharedPreferences {
-        return context.getSharedPreferences("preferences", Context.MODE_PRIVATE)
+        return context.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE)
+    }
+
+    fun getFirebaseToken(context: Context): String? {
+        return getPreferences(context).getString(FIREBASE_TOKEN, null)
+    }
+
+    fun setFirebaseToken(context: Context, token: String) {
+        val editor = getPreferencesEditor(context)
+        editor.putString(FIREBASE_TOKEN, token)
+        editor.apply()
+    }
+
+    private fun getPreferencesEditor(context: Context): SharedPreferences.Editor {
+        return getPreferences(context).edit()
     }
 
     fun setShowBGsPreferredView(context: Context, value: Boolean) {
