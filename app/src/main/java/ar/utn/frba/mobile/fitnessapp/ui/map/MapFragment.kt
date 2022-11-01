1330e96be10f626d3736bd9deb30d5189f39128d
facuhumpTenpo <facundo.humphreys@tenpo.cl>
2022-10-31 21:08:21 -0300

    Adding map. Markers still not working

--- app/src/main/java/ar/utn/frba/mobile/fitnessapp/ui/map/MapFragment.kt
+++ app/src/main/java/ar/utn/frba/mobile/fitnessapp/ui/map/MapFragment.kt
@@ -4,18 +4,25 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import ar.utn.frba.mobile.fitnessapp.MyPreferences
 import ar.utn.frba.mobile.fitnessapp.R
 import ar.utn.frba.mobile.fitnessapp.databinding.FragmentMapBinding
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-class MapFragment : Fragment() {
+class MapFragment : Fragment(), OnMapReadyCallback {
 
     private var _binding: FragmentMapBinding? = null
     private var showBG: Boolean = true
+    private lateinit var mMap: GoogleMap
+
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -26,29 +33,40 @@ class MapFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val mapViewModel =
-            ViewModelProvider(this).get(MapViewModel::class.java)
 
         _binding = FragmentMapBinding.inflate(inflater, container, false)
         val root: View = binding.root
+//        val mapFragment = activity?.supportFragmentManager?.findFragmentById(R.id.map) as SupportMapFragment
+//        mapFragment.getMapAsync(this)
+//        activity?.supportFragmentManager?.findFragmentById(R.id.map)
 
-        val textView: TextView = binding.textMap
-        mapViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
         return root
     }
 
     override fun onStart() {
         super.onStart()
         showBG = MyPreferences.isShowBGsPreferredView(context!!)
-        if(showBG){
-            activity?.findViewById<ConstraintLayout>(R.id.mapScreen)?.setBackgroundResource(R.drawable.bg_proteinx);
+        if (showBG) {
+            activity?.findViewById<ConstraintLayout>(R.id.mapScreen)
+                ?.setBackgroundResource(R.drawable.bg_proteinx)
         }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        // Add a marker in Sydney and move the camera
+        val sydney = LatLng(-34.0, 151.0)
+        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
     }
 }
