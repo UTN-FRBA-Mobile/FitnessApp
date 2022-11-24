9775c0aa8b359c3c48cfc0bd47f47739147a77da
Bade99 <31745377+Bade99@users.noreply.github.com>
2022-11-24 15:32:56 -0300

    Calendar widget with support for Events

--- app/src/main/java/ar/utn/frba/mobile/fitnessapp/ui/bookings/BookingsFragment.kt
+++ app/src/main/java/ar/utn/frba/mobile/fitnessapp/ui/bookings/BookingsFragment.kt
@@ -28,7 +28,6 @@ class BookingsFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(BookingsViewModel::class.java)
-        // TODO: Use the ViewModel
     }
 
 }
