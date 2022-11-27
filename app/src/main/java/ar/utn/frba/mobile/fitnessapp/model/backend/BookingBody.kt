e0ded19b637604bf64a38d7048123c3795cf64d7
facuhumpTenpo <facundo.humphreys@tenpo.cl>
2022-11-27 13:19:34 -0300

    Successfully booking class

--- app/src/main/java/ar/utn/frba/mobile/fitnessapp/model/backend/BookingBody.kt
+++
@@ -1,8 +0,0 @@
-package ar.utn.frba.mobile.fitnessapp.model.backend
-
-import com.google.gson.annotations.SerializedName
-
-data class BookingBody(
-    @SerializedName("user_id")
-    val userId: String
-)
--- app/src/main/java/ar/utn/frba/mobile/fitnessapp/ui/classes/AcceptDialogFragment.kt
+++ app/src/main/java/ar/utn/frba/mobile/fitnessapp/ui/classes/AcceptDialogFragment.kt
@@ -4,22 +4,35 @@ import android.app.AlertDialog
 import android.app.Dialog
 import android.content.DialogInterface
 import android.os.Bundle
+import android.widget.Toast
 import androidx.fragment.app.DialogFragment
 import androidx.navigation.fragment.navArgs
 import ar.utn.frba.mobile.fitnessapp.R
+import ar.utn.frba.mobile.fitnessapp.model.backend.BackendService
+import ar.utn.frba.mobile.fitnessapp.model.backend.call
 
 class AcceptDialogFragment : DialogFragment() {
     private val args: AcceptDialogFragmentArgs by navArgs()
+    private val backend: BackendService = BackendService.create()
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         return activity?.let {
             // Use the Builder class for convenient dialog construction
             val builder = AlertDialog.Builder(it)
             builder.setMessage(R.string.dialog_start_game)
-                .setPositiveButton(R.string.accept_gym,
-                    DialogInterface.OnClickListener { dialog, id ->
-                        // START THE GAME!
-                    })
+                .setPositiveButton(R.string.accept_gym
+                ) { dialog, id ->
+                    // START THE GAME!
+                    backend.reserve("1", args.gymClass.gymId, args.gymClass.id).call(
+                        onResponse = { _, response ->
+                            Toast.makeText(
+                                (dialog as AlertDialog).context,
+                                R.string.class_successfully_booked,
+                                Toast.LENGTH_LONG
+                            ).show()
+                            dialog.dismiss()
+                        })
+                }
                 .setNegativeButton(R.string.cancel,
                     DialogInterface.OnClickListener { dialog, id ->
                         // User cancelled the dialog
