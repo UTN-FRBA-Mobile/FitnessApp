48d55a6bf60fb4b57a03c7f710b35accd20dc3cc
Bade99 <31745377+Bade99@users.noreply.github.com>
2022-11-26 22:01:12 -0300

    GymDetails: Retrieving & showing gym's logo image from the backend

--- app/src/main/java/ar/utn/frba/mobile/fitnessapp/ui/details/DetailsFragment.kt
+++ app/src/main/java/ar/utn/frba/mobile/fitnessapp/ui/details/DetailsFragment.kt
@@ -1,9 +1,12 @@
 package ar.utn.frba.mobile.fitnessapp.ui.details
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
@@ -12,8 +15,13 @@ import ar.utn.frba.mobile.fitnessapp.R
 import ar.utn.frba.mobile.fitnessapp.databinding.FragmentClassesBinding
 import ar.utn.frba.mobile.fitnessapp.databinding.FragmentDetailsBinding
 import ar.utn.frba.mobile.fitnessapp.model.Gym
+import ar.utn.frba.mobile.fitnessapp.model.backend.BackendService
 import ar.utn.frba.mobile.fitnessapp.ui.classes.ClassesFragmentArgs
 import org.imaginativeworld.whynotimagecarousel.model.CarouselItem
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import java.util.Base64
 
 class DetailsFragment : Fragment() {
     private val args: DetailsFragmentArgs by navArgs()
@@ -26,6 +34,8 @@ class DetailsFragment : Fragment() {
     //private lateinit var viewModel: DetailsViewModel
 
     private var carouselHeight = R.dimen.details_img_height
+
+    private val backend = BackendService.create()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -44,7 +54,18 @@ class DetailsFragment : Fragment() {
         val gym: Gym = args.gym
 
         binding.detGymImages.setImageResource(R.drawable.bg_yoga)
-        binding.detGymLogo.setImageResource(R.drawable.bg_beachx) //TODO(fran):
+        binding.detGymLogo.setImageResource(R.drawable.bg_beachx)
+
+        backend.image(gym.id).enqueue(object : Callback<String> {
+            override fun onResponse(call: Call<String>, response: Response<String>) {
+                val b64img : String = response.body()!!
+                val decodedImg : ByteArray = android.util.Base64.decode(b64img, android.util.Base64.DEFAULT)
+                val img : Bitmap = BitmapFactory.decodeByteArray(decodedImg,0,decodedImg.size)
+                binding.detGymLogo.setImageBitmap(img)
+            }
+            override fun onFailure(call: Call<String>, t: Throwable) { Toast.makeText(activity, "No Gym Logo found!", Toast.LENGTH_SHORT).show() }
+        })
+
         binding.detGymName.text = gym.name
         binding.detGymContactnfo.text = "[AContact Info]"
         binding.detGymDesc.text = "ALorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
